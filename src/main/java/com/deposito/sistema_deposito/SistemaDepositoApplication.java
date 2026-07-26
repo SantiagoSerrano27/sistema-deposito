bced2bee734c5625efade9559cb5954004e6f00a
Santiago Serrano <santiagogonzaloserrano@gmail.com>
2026-07-26 19:30:17 -0300

    Setup inicial del proyecto con Spring Boot

---
+++ src/main/java/com/deposito/sistema_deposito/SistemaDepositoApplication.java
@@ -0,0 +1,13 @@
+package com.deposito.sistema_deposito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDepositoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDepositoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deposito/sistema_deposito/SistemaDepositoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deposito.sistema_deposito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDepositoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
